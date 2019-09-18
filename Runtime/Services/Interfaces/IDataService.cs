2d021c975af230783c6160586191618042128fe3
JuDelCo <judelco.dev@gmail.com>
2019-09-18 21:47:06 +0100

    Add DataService.

---
+++ Runtime/Services/Interfaces/IDataService.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace Ju
+{
+public interface IDataService : IService, ILoggableService
+{
+	void SetShared<T>(T obj);
+	T GetShared<T>() where T : class;
+	void RemoveShared<T>();
+
+	void Add<T>(T obj);
+	List<T> GetList<T>();
+	void Remove<T>(T obj);
+}
+}
---
+++ Runtime/Services/Providers/DataService/DataService.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+
+namespace Ju
+{
+public class DataService : IDataService
+{
+	public event LogMessageEvent OnLogDebug = delegate { };
+	public event LogMessageEvent OnLogInfo = delegate { };
+	public event LogMessageEvent OnLogNotice = delegate { };
+	public event LogMessageEvent OnLogWarning = delegate { };
+	public event LogMessageEvent OnLogError = delegate { };
+
+	private Dictionary<Type, object> sharedItems;
+	private Dictionary<Type, object> items;
+
+	public void Setup()
+	{
+		sharedItems = new Dictionary<Type, object>();
+		items = new Dictionary<Type, object>();
+	}
+
+	public void Start()
+	{
+	}
+
+	public void SetShared<T>(T obj)
+	{
+		var type = typeof(T);
+
+		if (sharedItems.ContainsKey(type))
+		{
+			OnLogWarning("A shared object of type {0} was already stored");
+		}
+
+		sharedItems[type] = obj;
+	}
+
+	public T GetShared<T>() where T : class
+	{
+		var type = typeof(T);
+		T instance = null;
+
+		if (sharedItems.ContainsKey(type))
+		{
+			instance = (T)sharedItems[type];
+		}
+
+		return instance;
+	}
+
+	public void RemoveShared<T>()
+	{
+		var type = typeof(T);
+
+		if (sharedItems.ContainsKey(type))
+		{
+			sharedItems.Remove(type);
+		}
+	}
+
+	public void Add<T>(T obj)
+	{
+		var type = typeof(T);
+		List<T> list = null;
+
+		if (!items.ContainsKey(type))
+		{
+			list = new List<T>();
+			items[type] = list;
+		}
+		else
+		{
+			list = (List<T>)items[type];
+		}
+
+		list.Add(obj);
+	}
+
+	public List<T> GetList<T>()
+	{
+		var type = typeof(T);
+		List<T> list = null;
+
+		if (items.ContainsKey(type))
+		{
+			list = (List<T>)items[type];
+		}
+
+		return list;
+	}
+
+	public void Remove<T>(T obj)
+	{
+		var type = typeof(T);
+
+		if (items.ContainsKey(type))
+		{
+			var list = (List<T>)items[type];
+
+			list.Remove(obj);
+
+			if (list.Count == 0)
+			{
+				items.Remove(type);
+			}
+		}
+	}
+}
+}
